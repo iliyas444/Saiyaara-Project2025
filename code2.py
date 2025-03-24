428ceef507f2a2a223bceb10a0bec9f6c8a6325b
iliyas444 <iliyas092m@gmail.com>
2025-03-24 23:02:43 +0300

    new file added main branch

---
+++ code2.py
@@ -0,0 +1,5 @@
+I am adding data and codes into the Git Hub
+in the folder
+git add data1.py   name of the folder you want to save the data
+git commit -m "new file added into the main branch"
+git push -u origin main
