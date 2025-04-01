dec3bbdb9ee39fbe31e7ed449c3a5c8fe1485158
iliyas444 <iliyas092m@gmail.com>
2025-04-01 08:15:06 +0300

    feature202 changes

--- application.java
+++ application.java
@@ -17,4 +17,20 @@ public class Calculator {
     public int getValue() {
         return this.value;
     }
+    #feature202 changes by vivek on october 2024 on reports modules
+    public class Example {
+        public static void main(String[] args) {
+            sum(3, 5);
+           
+            int number1 = 2;
+            int number2 = 4;
+           
+            sum(number1, number2);       
+        }
+       
+        public static void sum(int first, int second) {
+            System.out.println("" + first + " + " + second + " = " + (first+ second));
+        }
+    }
 }
+feature202 code end here
